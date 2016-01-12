517b352661db87102a7eb567488e19355d78976e
zjpleau <zjpleau@gmail.com>
2016-01-12 13:57:27 -0800

    Update mount_acd.sh
    Added mount unionfs

--- mount_acd.sh
+++ mount_acd.sh
@@ -1,3 +1,4 @@
 /usr/local/bin/acdcli mount /home/zachary/mounts/.acd-sorted
 cat /home/zachary/encfspwd | ENCFS6_CONFIG='/home/zachary/encfs.xml' encfs -S /home/zachary/mounts/.acd-sorted /home/zachary/mounts/acd-sorted
 cat /home/zachary/encfspwd | ENCFS6_CONFIG='/home/zachary/encfs.xml' encfs -S /home/zachary/mounts/.local-sorted /home/zachary/mounts/local-sorted
+unionfs-fuse -o cow /home/zachary/mounts/local-sorted=RW:/home/zachary/mounts/acd-sorted=RO /home/zachary/mounts/sorted/
